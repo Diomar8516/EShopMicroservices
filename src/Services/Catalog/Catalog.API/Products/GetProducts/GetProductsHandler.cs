e1be9c04dd9da69e68841f9e863ef876a58bfbe9
DiomarGomez <dgomez@conexusit.com>
2025-03-01 17:59:30 -0500

    metodo GetProcuts Final

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,4 +1,18 @@
-﻿namespace Catalog.API.Products.GetProducts;
-public class GetProductsHandler
+﻿
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+
+public class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsQueryHandler.handle called with {@Query}", query);
+
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+        return new GetProductsResult(products);
+    }
 }
